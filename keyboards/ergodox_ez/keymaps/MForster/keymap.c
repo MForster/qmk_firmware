4df1e1a41ad099473bca5e891a960ed9f34cac21
Michael Forster <forster@google.com>
2020-10-30 08:54:06 +0100

    Remove transitional control keys

--- keyboards/ergodox_ez/keymaps/MForster/keymap.c
+++ keyboards/ergodox_ez/keymaps/MForster/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,        LT(I3WSM,KC_Q),LT(I3WS, KC_W),LT(I3MV, KC_E),LT(I3, KC_R),  KC_T,          C(KC_KP_PLUS),
     KC_CAPSLOCK,   LT(NUM, KC_A), LT(FUNC, KC_S),LT(SEL, KC_D), LT(MOVE, KC_F),KC_G,
     KC_LSHIFT,     LCTL_T(KC_Z),  KC_X,          KC_C,          KC_V,          KC_B,          C(KC_KP_MINUS),
-    KC_LCTRL,      KC_LWIN,       _______,       _______,       KC_LALT,
+    _______,       KC_LWIN,       _______,       _______,       KC_LALT,
 
                                                                                C(KC_X),       C(KC_C),
                                                                                               C(KC_V),
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_VOLU,       KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_BSLASH,
                    KC_H,          KC_J,          KC_K,          KC_L,       LT(SYM, KC_SCLN), KC_QUOTE,
     KC_VOLD,       KC_N,          KC_M,          KC_COMMA,      KC_DOT,     RCTL_T(KC_SLASH), KC_RSHIFT,
-                                  KC_RALT,       _______,       _______,       KC_RWIN,       KC_RCTRL,
+                                  KC_RALT,       _______,       _______,       KC_RWIN,       _______,
 
     RESET,         KC_INSERT,
     TO(GAME),
